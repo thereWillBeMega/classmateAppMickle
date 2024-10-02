5ca3b22e8d105404b88dd2f90ae172e87156d4bf
Peter Mickle <peter.mickle26@d155.org>
2024-10-02 07:13:27 -0500

    Initial Commit

---
+++ classmateAppMickle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  classmateAppMickle
+//
+//  Created by PETER MICKLE on 10/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ classmateAppMickleTests/classmateAppMickleTests.swift
@@ -0,0 +1,17 @@
+//
+//  classmateAppMickleTests.swift
+//  classmateAppMickleTests
+//
+//  Created by PETER MICKLE on 10/2/24.
+//
+
+import Testing
+@testable import classmateAppMickle
+
+struct classmateAppMickleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
