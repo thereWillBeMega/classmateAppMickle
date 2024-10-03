4c0ff87f0abd2712bc40a141fec80dc1fdbfddb8
Peter Mickle <peter.mickle26@d155.org>
2024-10-03 08:08:28 -0500

    i commit!

---
+++ classmateAppMickle/Classmate.swift
@@ -0,0 +1,8 @@
+//
+//  Classmate.swift
+//  classmateAppMickle
+//
+//  Created by PETER MICKLE on 10/2/24.
+//
+
+import Foundation
---
+++ classmateAppMickle/ViewControllerStudents.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerStudents.swift
+//  classmateAppMickle
+//
+//  Created by PETER MICKLE on 10/2/24.
+//
+
+import UIKit
+
+class ViewControllerStudents: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
